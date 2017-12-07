3e871ae73b32b6d5ec99632b032f004ee2883108
Paula <paulamcarpenter@gmail.com>
2017-12-07 12:24:57 -0700

    Added Firebase and Moment.js

--- logic.js
+++ logic.js
@@ -17,8 +17,13 @@
     // Initial Values
     var nameTrain = "";
     var destTrain = "";
-    var firstTrain = 0;
-    var freqTrain = 0;
+    // var firstTrain = 0;
+    // var freqTrain = 0;
+
+    // Moment.js variables to be used for timing of Trains and Minutes Away
+    // var randomDate = "02/23/1999";
+    // var randomFormat = "MM/DD/YYYY";
+    // var convertedDate = moment(randomDate, randomFormat);
 
     // Capture Button Click
     $("#add-user").on("click", function() {
@@ -30,14 +35,14 @@
       // Don't forget to provide initial data to your Firebase database.
       nameTrain = $("#nameTrain-input").val();
       destTrain = $("#destTrain-input").val();
-      firstTrain = $("#firstTrain-input").val();
-      freqTrain = $("#freqTrain-input").val();
+      // firstTrain = $("#firstTrain-input").val();
+      // freqTrain = $("#freqTrain-input").val();
 
       database.ref().push({
         nameTrain: nameTrain,
         destTrain: destTrain,
-        firstTrain: firstTrain,
-        freqTrain: freqTrain
+        // firstTrain: firstTrain,
+        // freqTrain: freqTrain
       });
 
     });
@@ -45,18 +50,25 @@
     // Firebase watcher + initial loader HINT: .on("value")
     database.ref().on("child_added", function(snapshot) {
 
+      // Change the HTML to reflect
+      $("#nameTrain-display").append(snapshot.val().nameTrain);
+      $("#destTrain-display").append(snapshot.val().destTrain);
+      // $("#firstTrain-display").append(snapshot.val().firstTrain);
+      // $("#freqTrain-display").append(snapshot.val().freqTrain);
+
       // Log everything that's coming out of snapshot
       console.log(snapshot.val());
       console.log(snapshot.val().nameTrain);
       console.log(snapshot.val().destTrain);
-      console.log(snapshot.val().firstTrain);
-      console.log(snapshot.val().freqTrain);
+      // console.log(snapshot.val().firstTrain);
+      // console.log(snapshot.val().freqTrain);
 
-      // Change the HTML to reflect
-      $("#nameTrain-display").append(snapshot.val().nameTrain);
-      $("#destTrain-display").append(snapshot.val().destTrain);
-      $("#firstTrain-display").append(snapshot.val().firstTrain);
-      $("#freqTrain-display").append(snapshot.val().freqTrain);
+      // Add trains to list
+      $("#full-member-list").append("<tr><td ='nameTrain'> " + snapshot.val().nameTrain 
+        + " </tr><td ='destTrain'> " + snapshot.val().destTrain + "</tr>");
+        // + " </tr><td ='start'> " + snapshot.val().start 
+        // + " </tr><td ='rate'> " + snapshot.val().rate + " </tr>");
+
 
       // Handle the errors
     }, function(errorObject) {
