d919a2f9bb4c5cccf3d381d503bfbea97890ab16
Paula <paulamcarpenter@gmail.com>
2017-12-11 11:46:41 -0700

    Added moment.js

--- logic.js
+++ logic.js
@@ -1,76 +1,101 @@
-// (document).ready(function(){
-
-// Initialize Firebase
+  // Initialize Firebase
     var config = {
       apiKey: "AIzaSyCdPjuC2Ig3yoG9EYxKeuEdyp6Hw1DDCF0",
       authDomain: "train-time-a6f3b.firebaseapp.com",
       databaseURL: "https://train-time-a6f3b.firebaseio.com",
       projectId: "train-time-a6f3b",
-      storageBucket: "",
+      storageBucket: "train-time-a6f3b.appspot.com",
       messagingSenderId: "1042194730041"
     };
     firebase.initializeApp(config);
 
     // Create a variable to reference the database
-    var database = firebase.database();
+    var dataRef = firebase.database();
 
     // Initial Values
     var name = "";
     var dest = "";
-    var next = 0;
-    var freq = 0;
+    var next = "";
+    var freq = "";
+    var nextTrain = "";
+    var minutesAway = "";
+    var firstTimeConverted = "";
+    var currentTime = "";
+    var diffTime = "";
+    var tRemainder = "";
+    var minutesTilTrain = "";
+    var keyHolder = "";
+    var getKey = "";
+
+  $(document).ready(function() {
 
     // Moment.js variables to be used for timing of Trains and Minutes Away
-    // var randomDate = "02/23/1999";
-    // var randomFormat = "MM/DD/YYYY";
-    // var convertedDate = moment(randomDate, randomFormat);
-
     // Capture Button Click
-    $("#addUser").on("click", function() {
+    $("#add-train").on("click", function() {
       // Don't refresh the page!
       // event.preventDefault();
       // Code in the logic for storing and retrieving the most recent user.
-      name = $("#nameInput").val().trim();
-      dest = $("#destInput").val().trim();
-      next = $("#nextInput").val().trim();
-      freq = $("#freqInput").val().trim();
+      name = $("#name-input").val().trim();
+      dest = $("#dest-input").val().trim();
+      next = $("#next-input").val().trim();
+      freq = $("#freq-input").val().trim();
+        firstTimeConverted = moment(firstTrainTime, "hh.mm").subtract(1, "years");
+        currentTime = moment();
+        diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        tRemainder = diffTime % frequency;
+        minutesTilTrain = frequency - tRemainder;
+        nextTrain = moment().add(minutesTilTrain, "minutes");
+        nextTrainFormatted = moment(nextTrain).format("hh.mm");
 
-      database.ref().push({
+
+      // set to .set instead of .push to test
+      keyHolder = dataRef.push({
         name: name,
         dest: dest,
         next: next,
-        freq: freq
+        freq: freq,
+        nextTrainFormatted: nextTrainFormatted,
+        minutesTilTrain: minutesTilTrain
       });
 
-    });
+      $("#name-input").val("");
+      $("#dest-input").val("");
+      $("#next-input").val("");
+      $("#freq-input").val("");
 
-    // Firebase watcher + initial loader HINT: .on("value")
-    database.ref().on("child_added",function(snapshot) {
+      return false;
 
-      // Change the HTML to reflect.  Not sure if html or append or prepend here.
-      $("#nameDisplay").html(snapshot.val().name);
-      $("#destDisplay").html(snapshot.val().dest);
-      $("#nextDisplay").html(snapshot.val().next);
-      $("#freqDisplay").html(snapshot.val().freq);
+  });
 
-      // Log everything that's coming out of snapshot
-      console.log(snapshot.val());
-      console.log(snapshot.val().name);
-      console.log(snapshot.val().dest);
-      console.log(snapshot.val().next);
-      console.log(snapshot.val().freq);
+    // Firebase watcher + initial loader HINT: .on("child-added")
+dataRef.on("child_added",function(childSnapshot) {
 
-      // Add trains to list () Not sure if html or append or prepend here.
-      $("#full-member-list").append("<tr><td ='name'> " + snapshot.val().name 
-        + " </tr><td ='dest'> " + snapshot.val().dest
-        + " </tr><td ='next'> " + snapshot.val().next 
-        + " </tr><td ='freq'> " + snapshot.val().freq + " </tr>");
-
-
+ // Uncaught SyntaxError: missing ) after argument list line 74
+ $(".train-schedule").append("<tr class="table-row" id=" + "" + childSnapshot.key()) + "" + ">" +  $(".train-schedule").append("<tr class="table-row" id=" + "" + childSnapshot.key()">" +
+        "<td class="col-xs-3">" + childSnapshot.val().name +
+        "</td>" +
+        "<td class="col-xs-2">" + childSnapshot.val().dest +
+        "</td>" +
+        "<td class="col-xs-2">" + childSnapshot.val().freq +
+        "</td>" +
+        "<td class="col-xs-2">" + childSnapshot.val().nextTrainFormatted +
+        "</td>" +
+        "<td class="col-xs-2">" + childSnapshot.val().minutesTilTrain +
+        "</td>" +
+        "<td class="col-xs-1">" + "<input type="submit" value="remove train" class="remove-train btn btn-primary btn-sm">" + "</td>" +
+        "</tr>");  
+    
       // Handle the errors
-    }, function(errorObject) {
-         console.log("Errors handled: " + errorObject.code);
-       });
-  
+  }), function(errorObject) {
+         // console.log("Errors handled: " + errorObject.code);
+  };
 
+  $("body").on("click", ".remove-train", function() {
+  $(this).closest ("tr"),remove();
+  getKey = $(this).parent().parent().attr("id");
+  dataRef.child(getKey).remove();
+
+  });
+  
+//   $(".train-schedule").append("<tr class="table-row" id=" + "" + childSnapshot.key()) + "" + ">" +
 
